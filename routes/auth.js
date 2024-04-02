b22db92fb0fe517d21581825d6cf76a2b64d4242
yuki <yukitake2@icloud.com>
2024-04-03 00:13:08 +0900

    ログインのapi を作成

--- routes/auth.js
+++ routes/auth.js
@@ -18,5 +18,18 @@ router.post("/register", async (req, res) => {
 });
 
 //ログイン
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).json("存在しないユーザーです");
+
+    const ValidPassword = req.body.password === user.password;
+
+    if (!ValidPassword) return res.status(400).json("パスワードが違います");
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
