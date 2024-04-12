991718f8c2e83f1d1c3af885de946ec0684ff6e6
yuki <yukitake2@icloud.com>
2024-04-13 01:46:12 +0900

    色々変更した

--- routes/records.js
+++ routes/records.js
@@ -30,25 +30,49 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+//ここより下、ログインしている本人以外見えてはいけないから変える必要あいそう
+
 //記録の取得
-router.get("/:id", async (req, res) => {
-  try {
-    const record = await Record.findById(req.params.id);
-    return res.status(200).json(record);
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const record = await Record.findById(req.params.id);
+//     return res.status(200).json(record);
+//   } catch (err) {
+//     return res.status(500).json(err);
+//   }
+// });
 
 //ホームでの全ての記録の取得;
 router.get("/all/:username", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username });
-    const records = await Record.find({ userId: user._id });
-    return res.status(200).json(records);
+    if (user.password === req.body.password) {
+      const records = await Record.find({ userId: user._id });
+      return res.status(200).json(records);
+    } else {
+      return res.status(403).json("あなたは他人の記録を見ることはできません");
+    }
   } catch (err) {
     return res.status(500).json(err);
   }
 });
+// router.get("/all/:username", async (req, res) => {
+//   try {
+//     const user = await User.findOne({ username: req.params.username });
+//     const records = await Record.find({ userId: user._id });
+//     return res.status(200).json(records);
+//   } catch (err) {
+//     return res.status(500).json(err);
+//   }
+// });
+
+//日付と名前からその人の記録を取得する
+// router.get("/study/:/:date", async (req, res) => {
+//   try {
+//     // const record = await
+//   } catch (err) {
+//     return res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
